d23550a6ceedb4263fe03cabb5f01676a0ade654
John Garrett <infantrygarrett@icloud.com>
2019-04-27 22:28:22 -0500

    basic layout

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Banner.css";
+
+const Banner = () => (
+  <div className="banner text-center d-flex align-items-center justify-content-center">
+    <p className="m-0">
+    Time to get Schwifty---
+      Click on an image to earn points, but don't click on any more than once!
+    </p>
+  </div>
+);
+export default Banner;
